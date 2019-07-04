3411ec18c9109548db2e82cee16e5c8038dbd007
592595 <744151913@qq.com>
2019-07-04 21:00:33 +0800

    commit

--- 5.2-using-convnets-with-small-datasets.py
+++ 5.2-using-convnets-with-small-datasets.py
@@ -1,4 +1,10 @@
 import os, shutil
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
 
 # 1、下载数据
 # The path to the directory where the original
@@ -8,40 +14,40 @@ original_dataset_dir = 'Downloads/kaggle_original_data'
 # The directory where we will
 # store our smaller dataset
 base_dir = 'Downloads/cats_and_dogs_small'
-os.mkdir(base_dir)
+# os.mkdir(base_dir)
 
 # Directories for our training,
 # validation and test splits
 train_dir = os.path.join(base_dir, 'train')
-os.mkdir(train_dir)
+# os.mkdir(train_dir)
 validation_dir = os.path.join(base_dir, 'validation')
-os.mkdir(validation_dir)
+# os.mkdir(validation_dir)
 test_dir = os.path.join(base_dir, 'test')
-os.mkdir(test_dir)
+# os.mkdir(test_dir)
 
 # Directory with our training cat pictures
 train_cats_dir = os.path.join(train_dir, 'cats')
-os.mkdir(train_cats_dir)
+# os.mkdir(train_cats_dir)
 
 # Directory with our training dog pictures
 train_dogs_dir = os.path.join(train_dir, 'dogs')
-os.mkdir(train_dogs_dir)
+# os.mkdir(train_dogs_dir)
 
 # Directory with our validation cat pictures
 validation_cats_dir = os.path.join(validation_dir, 'cats')
-os.mkdir(validation_cats_dir)
+# os.mkdir(validation_cats_dir)
 
 # Directory with our validation dog pictures
 validation_dogs_dir = os.path.join(validation_dir, 'dogs')
-os.mkdir(validation_dogs_dir)
+# os.mkdir(validation_dogs_dir)
 
 # Directory with our validation cat pictures
 test_cats_dir = os.path.join(test_dir, 'cats')
-os.mkdir(test_cats_dir)
+# os.mkdir(test_cats_dir)
 
 # Directory with our validation dog pictures
 test_dogs_dir = os.path.join(test_dir, 'dogs')
-os.mkdir(test_dogs_dir)
+# os.mkdir(test_dogs_dir)
 
 # Copy first 1000 cat images to train_cats_dir
 fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
@@ -91,3 +97,215 @@ print('total validation cat images:', len(os.listdir(validation_cats_dir)))
 print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
 print('total test cat images:', len(os.listdir(test_cats_dir)))
 print('total test dog images:', len(os.listdir(test_dogs_dir)))
+
+# # 2、构建网络
+# # 注意，功能图的深度在网络中逐渐增加（从32到128），而功能图的大小则在减小（从148x148到7x7）
+# # 这是几乎所有convnet都能中看到的模式。
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3, 3), activation='relu',
+#                         input_shape=(150, 150, 3)))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(512, activation='relu'))
+# model.add(layers.Dense(1, activation='sigmoid'))
+# print(model.summary())
+# # 对于编译步骤，我们将像往常一样使用RMSprop优化器,loss的选择：binary crossentropy
+# model.compile(loss='binary_crossentropy',
+#               optimizer=optimizers.RMSprop(lr=1e-4),
+#               metrics=['acc'])
+#
+# # 3、数据预处理
+# # 将数据格式化为适当的预处理浮点张量。
+# # 1）	阅读图片文件。
+# # 2）	将jpeg内容解码为像素的RBG网格。
+# # 3）	把它们转换成浮点张量。
+# # 4）	将像素值（介于0和255之间）重新调整为[0，1]间隔（神经网络擅长处理较小的输入值）。
+# # 使用keras图像处理助手工具的模块，位于keras.preprocessing.image。
+# # All images will be rescaled by 1./255
+# train_datagen = ImageDataGenerator(rescale=1. / 255)
+# test_datagen = ImageDataGenerator(rescale=1. / 255)
+#
+# train_generator = train_datagen.flow_from_directory(
+#     # This is the target directory
+#     train_dir,
+#     # All images will be resized to 150x150
+#     target_size=(150, 150),
+#     batch_size=20,
+#     # Since we use binary_crossentropy loss, we need binary labels
+#     class_mode='binary')
+#
+# validation_generator = test_datagen.flow_from_directory(
+#     validation_dir,
+#     target_size=(150, 150),
+#     batch_size=20,
+#     class_mode='binary')
+# # 让我们来看看其中一个生成器的输出：
+# for data_batch, labels_batch in train_generator:
+#     print('data batch shape:', data_batch.shape)
+#     print('labels batch shape:', labels_batch.shape)
+#     break
+#
+# # # 它生成一批150x150 RGB图像（shape（20，150，150，3））和二进制标签（shape（20，3））20是每批样品的数量（批量大小）。
+# # # 注意，生成器无限期地生成这些批：它只是在目标文件夹中的图像上无限循环。出于这个原因，我们需要在某个时刻中断迭代循环。
+# #
+# # 使用fit_generator的方法实现生成器将模型与数据相拟合
+# history = model.fit_generator(
+#     train_generator,
+#     # 由于数据正在不断地生成，生成器需要知道在声明一个时代结束之前，要从生成器中提取多少样本
+#     # steps_per_epoch的作用：从生成器中绘制steps-per-epoch批之后，即运行steps-per-epoch梯度下降步骤之后，拟合过程将进入下一个epoch。
+#     # 在我们的例子中，批量是20个样本大，所以需要100个批次，直到我们看到2000个样本的目标。
+#     steps_per_epoch=100,
+#     epochs=30,
+#     # validation_data这个参数本身可以是数据生成器，但也可以是numpy数组的元组。
+#     validation_data=validation_generator,
+#     # 如果将生成器作为验证数据传递，那么该生成器将无限地生成一批验证数据，因此我们还应指定validation-steps参数，该参数指示流程从验证生成器中抽取多少批进行评估。
+#     validation_steps=50)
+#
+# # 训练后始终保存我们的模型是一个很好的做法：
+# model.save('cats_and_dogs_small_1.h5')
+#
+# # 让我们用训练和验证数据来绘制模型在训练期间的loss 和accuracy ：
+# acc = history.history['acc']
+# val_acc = history.history['val_acc']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+#
+# epochs = range(len(acc))
+#
+# plt.plot(epochs, acc, 'bo', label='Training acc')
+# plt.plot(epochs, val_acc, 'b', label='Validation acc')
+# plt.title('Training and validation accuracy')
+# plt.legend()
+#
+# plt.figure()
+#
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.legend()
+#
+# plt.show()
+# 由于训练样本相对较少，出现了过度拟合的现象，采用数据增强的方法来缓解过度拟合。
+# 4、数据增强
+datagen = ImageDataGenerator(
+      rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+# 增强图像：
+# This is module with image preprocessing utilities
+from keras.preprocessing import image
+
+fnames = [os.path.join(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
+
+# We pick one image to "augment"
+img_path = fnames[3]
+
+# Read the image and resize it
+img = image.load_img(img_path, target_size=(150, 150))
+
+# Convert it to a Numpy array with shape (150, 150, 3)
+x = image.img_to_array(img)
+
+# Reshape it to (1, 150, 150, 3)
+x = x.reshape((1,) + x.shape)
+
+# The .flow() command below generates batches of randomly transformed images.
+# It will loop indefinitely, so we need to `break` the loop at some point!
+i = 0
+for batch in datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i += 1
+    if i % 4 == 0:
+        break
+
+plt.show()
+
+# 为了进一步防止过度拟合，我们还将在模型中添加一个退出层，就在密接分类器之前：
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+# 使用数据扩充和退出来训练我们的网络：
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,)
+
+# Note that the validation data should not be augmented!
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        # This is the target directory
+        train_dir,
+        # All images will be resized to 150x150
+        target_size=(150, 150),
+        batch_size=32,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=100,
+      validation_data=validation_generator,
+      validation_steps=50)
+
+model.save('cats_and_dogs_small_2.h5')
+# 重新绘制结果：
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
